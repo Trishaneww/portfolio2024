2405acd8b3aae9ff579d58f14c016c2dcf48ee83
Trishane <trishaneww@gmail.com>
2024-07-06 02:14:51 -0400

    foodbtyte tech stack

--- src/components/projects/Foodbyte.jsx
+++ src/components/projects/Foodbyte.jsx
@@ -1,7 +1,6 @@
 import './Projects.scss';
 import Navbar from '../navbar/Navbar';
 import Image1 from '../../images/foodbyte1.png';
-import Image2 from '../../images/foodbyte2.png';
 import Image3 from '../../images/foodbyte3.png';
 import Image4 from '../../images/foodbyte4.png';
 import Image5 from '../../images/foodbyte5.png';
@@ -17,10 +16,19 @@ const Foodbyte = () => {
             <img src={Image7} alt="preview image" />
 
             <section className="projectPage-gallery1">
+                <img src={Image3} alt="first image" />
                 <img src={Image4} alt="first image" />
                 <img src={Image5} alt="second image" />
                 <img src={Image6} alt="third image" />
             </section>
+
+            <section className="projectPage-gallery2">
+                <img src={Image1} alt="login page" />
+            </section>
+
+            <footer className="projectPage-nextProject">
+                <a href="/studyhive">StudyHive</a>
+            </footer>
         </div>
     </div>
   )
